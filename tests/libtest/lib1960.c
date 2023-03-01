c6dd54ea12baad65f6e5a65d6601cdc6c55d1a78
Daniel Stenberg <daniel@haxx.se>
2023-03-01 13:15:49 +0100

    fixup lib1960 for torture test and socket close

--- tests/libtest/lib1960.c
+++ tests/libtest/lib1960.c
@@ -27,6 +27,14 @@
 
 #include "memdebug.h"
 
+/* to prevent libcurl from closing our socket */
+static int closesocket_cb(void *clientp, curl_socket_t item)
+{
+  (void)clientp;
+  (void)item;
+  return 0;
+}
+
 /* provide our own socket */
 static curl_socket_t socket_cb(void *clientp,
                                curlsocktype purpose,
@@ -52,12 +60,10 @@ static int sockopt_cb(void *clientp,
 /* Expected args: URL IP PORT */
 int test(char *URL)
 {
-  CURL *curl;
+  CURL *curl = NULL;
   CURLcode res = TEST_ERR_MAJOR_BAD;
-  struct curl_slist *list = NULL;
-  struct curl_slist *connect_to = NULL;
   int status;
-  int client_fd;
+  int client_fd = -1;
   struct sockaddr_in serv_addr;
   unsigned short port = (unsigned short)atoi(libtest_arg3);
 
@@ -74,7 +80,7 @@ int test(char *URL)
   client_fd = socket(AF_INET, SOCK_STREAM, 0);
   if(client_fd < 0) {
     fprintf(stderr, "socket creation error\n");
-    return 1;
+    goto test_cleanup;
   }
 
   serv_addr.sin_family = AF_INET;
@@ -82,20 +88,19 @@ int test(char *URL)
 
   if(inet_pton(AF_INET, libtest_arg2, &serv_addr.sin_addr) <= 0) {
     fprintf(stderr, "inet_pton failed\n");
-    return 2;
+    goto test_cleanup;
   }
 
   status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
   if(status < 0) {
     fprintf(stderr, "connection failed\n");
-    return 3;
+    goto test_cleanup;
   }
 
   curl = curl_easy_init();
   if(!curl) {
     fprintf(stderr, "curl_easy_init() failed\n");
-    curl_global_cleanup();
-    return TEST_ERR_MAJOR_BAD;
+    goto test_cleanup;
   }
 
   test_setopt(curl, CURLOPT_VERBOSE, 1L);
@@ -103,6 +108,8 @@ int test(char *URL)
   test_setopt(curl, CURLOPT_OPENSOCKETDATA, &client_fd);
   test_setopt(curl, CURLOPT_SOCKOPTFUNCTION, sockopt_cb);
   test_setopt(curl, CURLOPT_SOCKOPTDATA, NULL);
+  test_setopt(curl, CURLOPT_CLOSESOCKETFUNCTION, closesocket_cb);
+  test_setopt(curl, CURLOPT_CLOSESOCKETDATA, NULL);
   test_setopt(curl, CURLOPT_VERBOSE, 1L);
   test_setopt(curl, CURLOPT_HEADER, 1L);
   test_setopt(curl, CURLOPT_URL, URL);
@@ -110,9 +117,8 @@ int test(char *URL)
   res = curl_easy_perform(curl);
 
 test_cleanup:
-
-  curl_slist_free_all(connect_to);
-  curl_slist_free_all(list);
+  if(client_fd >= 0)
+    sclose(client_fd);
   curl_easy_cleanup(curl);
   curl_global_cleanup();
 
